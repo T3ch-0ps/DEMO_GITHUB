2c7fd8d313183c70796de7ac3a79c471cef9ad93
T3ch-0ps <87118992+T3ch-0ps@users.noreply.github.com>
2021-07-08 14:28:37 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,19 @@
+#add
+def add(x,y):
+    return x+y 
+    
+# mul
+def mul(x,y):
+    return x*y  
+    
+#div 
+def div(x,y):
+ return x/y     //created on master agter branching bug456
+
+    return x*y
+    
+    
+#sub
+def sub(x,y):
+    return x-y
+
